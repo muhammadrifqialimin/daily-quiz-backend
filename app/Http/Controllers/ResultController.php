dda26a1ce0f5a180e7e01301724c49b5d533655f
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-02-11 16:42:06 +0700

    memperbaiki fungsional result controller serta menambahkan fungsi agar bisa melihat riwayat soal

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -8,13 +8,16 @@ use App\Models\Result;
 
 class ResultController extends Controller
 {
+    // --- FUNGSI 1: SUBMIT JAWABAN (UPDATE) ---
     public function submit(Request $request)
     {
         $studentId = $request->student_id;
         $category = $request->category;
+        
         $studentAnswers = $request->answers; 
 
         $totalCorrect = 0;
+        
         $totalQuestions = count($studentAnswers);
 
         foreach ($studentAnswers as $quizId => $answer) {
@@ -27,12 +30,14 @@ class ResultController extends Controller
 
         $finalScore = ($totalQuestions > 0) ? round(($totalCorrect / $totalQuestions) * 100) : 0;
 
+        // SIMPAN KE DATABASE
         Result::create([
             'student_id' => $studentId,
             'category' => $category,
             'total_correct' => $totalCorrect,
             'total_questions' => $totalQuestions,
-            'score' => $finalScore
+            'score' => $finalScore,
+            'answers' => json_encode($studentAnswers)
         ]);
 
         return response()->json([
@@ -41,6 +46,48 @@ class ResultController extends Controller
             'score' => $finalScore,
             'correct' => $totalCorrect,
             'total' => $totalQuestions
+        ]);
+    }
+
+    public function show($id)
+    {
+        $result = Result::find($id);
+
+        if (!$result) {
+            return response()->json(['message' => 'Data tidak ditemukan'], 404);
+        }
+        $studentAnswers = json_decode($result->answers, true) ?? [];
+        
+        $quizIds = array_keys($studentAnswers);
+        
+        $quizzes = Quiz::whereIn('id', $quizIds)->get();
+
+        $evaluation = $quizzes->map(function($q) use ($studentAnswers) {
+            $myAns = strtolower($studentAnswers[$q->id] ?? '-');
+            $correctAns = strtolower($q->answer);
+            $isCorrect = $myAns == $correctAns;
+
+            return [
+                'question' => $q->question,
+                'options' => [
+                    'a' => $q->option_a, 
+                    'b' => $q->option_b, 
+                    'c' => $q->option_c, 
+                    'd' => $q->option_d
+                ],
+                'my_answer' => $myAns,
+                'correct_answer' => $correctAns,
+                'is_correct' => $isCorrect
+            ];
+        });
+
+        return response()->json([
+            'status' => true,
+            'data' => [
+                'score' => $result->score,
+                'date' => $result->created_at->format('d M Y, H:i'),
+                'details' => $evaluation
+            ]
         ]);
     }
 }
